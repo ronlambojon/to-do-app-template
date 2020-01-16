10be33f2af02eefd9c7404e10124f599a9eb00e3
unknown <ron.lambojon@gmail.com>
2020-01-16 20:26:55 +0800

    adding edit and delete icons on ToDo Item.

--- src/components/to-do-list.component.jsx
+++ src/components/to-do-list.component.jsx
@@ -10,8 +10,11 @@ const ToDoList = () => (
         <li className='list-group-item text-capitalize d-flex justify-content-between my-2'>
           <h6> ToDo Item </h6>
           <div className='todo-icon'>
+            <span className='mx-2 text-success'>
+              <i className='fas fa-pen' />
+            </span>
             <span className='mx-2 text-danger'>
-              <i></i>
+              <i className='fas fa-trash' />
             </span>
           </div>
         </li>
